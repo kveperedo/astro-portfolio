6845f16ad11e3a04f9aaa86ff62e7aebe82c1f87
Kevin Peredo <kveperedo@gmail.com>
2023-03-21 21:00:49 +0800

    [fix] Revert viewport height correction
    - Reverted the viewport height correction as it was causing layout
    issues on mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -36,18 +36,6 @@ module.exports = {
                 },
             },
         },
-        height: (theme) => ({
-            auto: 'auto',
-            ...theme('spacing'),
-            full: '100%',
-            screen: 'calc(var(--vh) * 100)',
-        }),
-        minHeight: (theme) => ({
-            0: '0',
-            ...theme('spacing'),
-            full: '100%',
-            screen: 'calc(var(--vh) * 100)',
-        }),
     },
     plugins: [],
 };
